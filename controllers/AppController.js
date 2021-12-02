edf65f481ddd8a84c4d13822d99eeb7d60f82905
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-02 17:38:25 +1100

    feat : direct mail send function added

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -66,6 +66,15 @@ const AppController = {
             });
             await DeliverabilityInsights.service.create(Query_builder);
           }); */
+          let Query_builder            = {};
+          Query_builder.account_id     = account._id;
+          Query_builder.recipient      = to;
+          Mailer.send(mailData).then(function(mailresponse){
+            Query_builder.status   = 'sent';
+          }).then(function(error){
+            Query_builder.status   = 'failed';
+          });
+          await DeliverabilityInsights.service.create(Query_builder);
           response.status(200).json({
             type : AppConstants.RESPONSE_SUCCESS,
             message:  'Email has been sent successfully',
