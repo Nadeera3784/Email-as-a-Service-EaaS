b364bd1b08a05f8d4bfc1b1b0215e82cb89829be
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-06 18:51:16 +1100

    feat : redis test

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -10,7 +10,7 @@ const AccountTemplate                    = require('../services/AccountTemplate'
 const CustomSmtp                         = require('../services/CustomSmtp');
 const {AuthenticationTokenGenerate, AuthenticationParseUser}      = require('../services/Authentication');
 
-//const sendMailQueue = new bullQueue('sendMailQueue', { redis: { port: config_cache.cache.redis_port, host: config_cache.cache.redis_host}});
+const sendMailQueue = new bullQueue('sendMailQueue', { redis: { port: config_cache.cache.redis_port, host: config_cache.cache.redis_host}});
 
 
 const AppController = {
@@ -67,9 +67,7 @@ const AppController = {
               password : smtp.password
             }
           }
-
-          console.log(mailData);
-       /*    sendMailQueue.add(mailData);   
+          sendMailQueue.add(mailData);   
           sendMailQueue.process(async function (job) {
             let Query_builder            = {};
             Query_builder.account_id     = account._id;
@@ -80,17 +78,17 @@ const AppController = {
               Query_builder.status   = 'failed';
             });
             await DeliverabilityInsights.service.create(Query_builder);
-          }); */
-          var Query_builder            = {};
-          Query_builder.account_id     = account._id;
-          Query_builder.recipient      = to;
-          const is_send = await Mailer.send(mailData);
-          if(is_send){
-            Query_builder.status   = 'sent';
-          }else{
-            Query_builder.status   = 'failed';
-          }
-          await DeliverabilityInsights.service.create(Query_builder);
+          });
+          // var Query_builder            = {};
+          // Query_builder.account_id     = account._id;
+          // Query_builder.recipient      = to;
+          // const is_send = await Mailer.send(mailData);
+          // if(is_send){
+          //   Query_builder.status   = 'sent';
+          // }else{
+          //   Query_builder.status   = 'failed';
+          // }
+          //await DeliverabilityInsights.service.create(Query_builder);
           response.status(200).json({
             type : AppConstants.RESPONSE_SUCCESS,
             message:  'Email has been sent successfully',
