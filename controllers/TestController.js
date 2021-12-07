b8db8b374f4b570805f5b9b0f07fbcf599750e5d
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-07 15:10:15 +1100

    test : documentdb uri test case 02

--- controllers/TestController.js
+++ controllers/TestController.js
@@ -1,25 +1,27 @@
-var mongoose  = require('mongoose');
+var mongoose = require('mongoose');
 
+const db_uri = "mongodb://nadeeradocumentdb:na998743deer837483jlkrsd@o2o-caas-db.cluster-capzd9fgxyee.ap-southeast-1.docdb.amazonaws.com:27017/o2o-caas?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 const TestController = {
 
-    async index(request, response, next){
-        mongoose.connect("mongodb://nadeeradocumentdb:na998743deer837483Jlkrsd@o2o-caas-db.cluster-capzd9fgxyee.ap-southeast-1.docdb.amazonaws.com:27017/o2o-caas?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false",
-         {useNewUrlParser: true, ssl: true, sslValidate: false, sslCA: `rds-combined-ca-bundle.pem`}).then(function(res){
-            console.log('connected');
-            response.status(200).json({
-                type : 'success',
-                message:  'Account has been updated successfully',
-                data:  res
-              });
-              return;
-        }).catch(function(error){
-            response.status(400).json({
-                type : 'error',
-                message:  'Something went wrong, please try again later',
-                data: error
-            });
-            return;
-        });
+    async index(request, response, next) {
+
+        if (!mongoose.connection.readyState) {
+            await mongoose.connect(
+                db_uri,
+                {
+                    tlsCAFile: `rds-combined-ca-bundle.pem`,
+                },
+                function (err, client) {
+                    if (err) {
+                        console.log(err);
+                        response.status(400).json({
+                            type: 'error',
+                            message: 'Something went wrong, please try again later',
+                            data: error
+                        });
+                    }
+                },
+        }
     },
 }
 
