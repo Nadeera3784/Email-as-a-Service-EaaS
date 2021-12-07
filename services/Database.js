6545f675ead9c4f2502a464299200a652d926e18
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-07 16:39:21 +1100

    test : readyState connection test added

--- services/Database.js
+++ services/Database.js
@@ -3,11 +3,13 @@ const mongoose   = require('mongoose');
 const config_app = require('../config/app.js');
 
 const databaseInitializer = async function (uri) {
-    mongoose.connect(uri, {
-        tlsCAFile: `rds-combined-ca-bundle.pem`
-    });
+    if (!mongoose.connection.readyState) {
+        await mongoose.connect(uri, {
+            tlsCAFile: `rds-combined-ca-bundle.pem`
+        });
+     }
 
-    if(config_app.app.environment == "development"){
+/*     if(config_app.app.environment == "development"){
         mongoose.connection.on('connected', function () {  
             console.log(`Database connection open to ${mongoose.connection.host} ${mongoose.connection.name}`);
         }); 
@@ -19,7 +21,7 @@ const databaseInitializer = async function (uri) {
         mongoose.connection.on('disconnected', function () {  
         console.log('Mongoose default connection disconnected'); 
         });
-    }
+    } */
 }
 
 module.exports = {
