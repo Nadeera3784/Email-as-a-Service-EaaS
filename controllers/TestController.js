7c33c459a9d459f8fbbc5e0160c07fc5dcdb69f8
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-08 16:26:18 +1100

    test : mongoose ocnfig updated

--- controllers/TestController.js
+++ controllers/TestController.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var fs = require('fs');
 const {Account_Model} = require('../models');
 
 const db_uri = "mongodb://nadeeradocumentdb:na998743deer837483jlkrsd@o2o-caas-db-public.cluster-capzd9fgxyee.ap-southeast-1.docdb.amazonaws.com:27017/o2o-caas?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
@@ -6,18 +7,27 @@ const db_uri = "mongodb://nadeeradocumentdb:na998743deer837483jlkrsd@o2o-caas-db
 const TestController = {
 
     async index(request, response, next) {
-        if (!mongoose.connection.readyState) {
-            await mongoose.connect(db_uri,{tlsCAFile: `rds-combined-ca-bundle.pem`}, function (err, client) {
-                if (err) {
-                    console.log('debug error', err);
-                    response.status(400).json({
-                        type: 'error',
-                        message: 'Something went wrong, please try again later',
-                        data: err
-                    });
-                }
-            });
-        }
+
+        mongoose.connect(db_uri, {
+            useNewUrlParser: true,
+            ssl: true,
+            sslValidate: false,
+            sslCA: fs.readFileSync('./rds-combined-ca-bundle.pem')})
+        .then(() => console.log('Connection to DB successful'))
+        .catch((err) => console.error(err,'Error'));
+
+        // if (!mongoose.connection.readyState) {
+        //     await mongoose.connect(db_uri,{tlsCAFile: `rds-combined-ca-bundle.pem`}, function (err, client) {
+        //         if (err) {
+        //             console.log('debug error', err);
+        //             response.status(400).json({
+        //                 type: 'error',
+        //                 message: 'Something went wrong, please try again later',
+        //                 data: err
+        //             });
+        //         }
+        //     });
+        // }
 
         const am = await Account_Model.create({email : 'john doe', type : 'type_01', email_service : 'ses'});
         
