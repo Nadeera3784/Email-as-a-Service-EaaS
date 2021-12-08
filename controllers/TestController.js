d97c76373843b2a41ece24b49c7740b853577b4b
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-08 16:32:04 +1100

    test : key fiel path updated

--- controllers/TestController.js
+++ controllers/TestController.js
@@ -12,7 +12,7 @@ const TestController = {
             useNewUrlParser: true,
             ssl: true,
             sslValidate: false,
-            sslCA: fs.readFileSync('./rds-combined-ca-bundle.pem')})
+            sslCA: `rds-combined-ca-bundle.pem`})
         .then(() => console.log('Connection to DB successful'))
         .catch((err) => console.error(err,'Error'));
 
