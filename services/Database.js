0aa77e2df3cb47c3429a75e9e246572db0c6df46
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-08 13:52:43 +1100

    test : readystate removed

--- services/Database.js
+++ services/Database.js
@@ -29,7 +29,7 @@ const mongodbInitializer = async (request, response, next) => {
     
     const db_uri = "mongodb://nadeeradocumentdb:na998743deer837483jlkrsd@o2o-caas-db.cluster-capzd9fgxyee.ap-southeast-1.docdb.amazonaws.com:27017/o2o-caas?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 
-    if (!mongoose.connection.readyState) {
+    //if (!mongoose.connection.readyState) {
         await mongoose.connect(db_uri,{tlsCAFile: `rds-combined-ca-bundle.pem`}, function (err, client) {
             if (err) {
                 console.log('debug error', err);
@@ -40,7 +40,7 @@ const mongodbInitializer = async (request, response, next) => {
                 });
             }
         });
-    }
+    //}
 
     next();
 };
