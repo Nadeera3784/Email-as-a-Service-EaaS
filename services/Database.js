ef049edf18866c8c33973d8a4de0a6791c271f5d
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-08 13:58:17 +1100

    feat : useNewUrlParser added to the mongodb middleware

--- services/Database.js
+++ services/Database.js
@@ -30,7 +30,7 @@ const mongodbInitializer = async (request, response, next) => {
     const db_uri = "mongodb://nadeeradocumentdb:na998743deer837483jlkrsd@o2o-caas-db.cluster-capzd9fgxyee.ap-southeast-1.docdb.amazonaws.com:27017/o2o-caas?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 
     //if (!mongoose.connection.readyState) {
-        await mongoose.connect(db_uri,{tlsCAFile: `rds-combined-ca-bundle.pem`}, function (err, client) {
+        await mongoose.connect(db_uri,{useNewUrlParser: true, tlsCAFile: `rds-combined-ca-bundle.pem`}, function (err, client) {
             if (err) {
                 console.log('debug error', err);
                 response.status(400).json({
