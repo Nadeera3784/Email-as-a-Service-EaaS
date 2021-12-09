c9caf3864453d502521fb06fceace5f6a040274a
Nadeera Sampath <Nadeera3784@gmail.com>
2021-12-09 18:30:27 +1100

    test : test redis que

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -286,10 +286,32 @@ const AppController = {
     async redisTest (request, response, next){
       const videoQueue = new Queue('video transcoding', 'redis://o2o-caas-dev.6yuluw.ng.0001.apse1.cache.amazonaws.com:6379');
 
+
+      videoQueue.process(function (job, done) {
+
+        // job.data contains the custom data passed when the job was created
+        // job.id contains id of this job.
+      
+        // transcode video asynchronously and report progress
+        job.progress(42);
+      
+        // call done when finished
+        done();
+      
+        // or give a error if error
+        done(new Error('error transcoding'));
+      
+        // or pass it a result
+        done(null, { framerate: 29.5 /* etc... */ });
+      
+        // If the job throws an unhandled exception it is also handled correctly
+        throw new Error('some unexpected error');
+      });
+
+      
       return response.status(200).json({
         type : AppConstants.RESPONSE_SUCCESS,
         message:  'Redis test successfully',
-        data:  videoQueue
       }); 
     }
     
