2303ba38f2d5fbbb79bc204521913aa01aa0e5ab
Nadeera Sampath <Nadeera3784@gmail.com>
2022-01-17 15:35:15 +1100

    test : reset template collection

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -332,7 +332,7 @@ const AppController = {
       await AccountTemplate.service.deleteAll();
       await CustomSmtp.service.deleteAll();
       await DeliverabilityInsights.service.deleteAll();
-      //await Templates.service.deleteAll();
+      await Templates.service.deleteAll();
       return response.status(200).json({
         type : AppConstants.RESPONSE_SUCCESS,
         message:  'Database has been reset successfully',
